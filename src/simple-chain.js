8159e06ba32ec75713779dcff269e958cd382360
Dim Morpheus <dimmorpheus@icloud.com>
2021-03-25 12:20:47 +0300

    init: add simple-chain script

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,25 +1,35 @@
 const CustomError = require("../extensions/custom-error");
 
 const chainMaker = {
+
+	chain: [],
+
   getLength() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+		return this.chain.length;
   },
-  addLink(value) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  addLink(value = "") {
+		if (value === "") 
+			this.chain.push("( )");
+		else
+			this.chain.push(`( ${value} )`);
+		return this;
   },
   removeLink(position) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+		if (this.chain[position] == undefined) {
+			this.chain = [];
+			throw new Error();
+		}
+		this.chain.splice(position - 1, 1);
+		return this;
   },
   reverseChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+		this.chain.reverse();
+		return this;
   },
   finishChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+		var c = this.chain.join("~~");
+		this.chain = [];
+		return c;
   }
 };
 
